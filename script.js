5541c91520c5f5b40ca93a1118b406c1893e7d41
Thayane Batista <thayane.batista.ec@gmail.com>
2021-07-12 21:12:05 -0400

    first commit
    adding the test files

---
+++ script.js
@@ -0,0 +1,394 @@
+const api_key = 'api_key=53905adf7ee1497e30bc06279d32e46e';
+const url_base = 'https://api.themoviedb.org/3';
+const url_lang = '&language=pt-BR';
+const url_img = 'https://image.tmdb.org/t/p/w500';
+const url_api = url_base + '/discover/movie?sort_by=popularity.desc&' + api_key + url_lang;
+
+const url_search = url_base + '/search/movie?' + api_key;
+
+const genres = [
+    {
+        "id": 28,
+        "name": "Ação"
+    },
+    {
+        "id": 12,
+        "name": "Aventura"
+    },
+    {
+        "id": 16,
+        "name": "Animação"
+    },
+    {
+        "id": 35,
+        "name": "Comédia"
+    },
+    {
+        "id": 80,
+        "name": "Crime"
+    },
+    {
+        "id": 99,
+        "name": "Documentários"
+    },
+    {
+        "id": 18,
+        "name": "Drama"
+    },
+    {
+        "id": 10751,
+        "name": "Família"
+    },
+    {
+        "id": 14,
+        "name": "Fantasia"
+    },
+    {
+        "id": 36,
+        "name": "Historia"
+    },
+    {
+        "id": 27,
+        "name": "Horror"
+    },
+    {
+        "id": 10402,
+        "name": "Musical"
+    },
+    {
+        "id": 9648,
+        "name": "Mistério"
+    },
+    {
+        "id": 10749,
+        "name": "Romance"
+    },
+    {
+        "id": 878,
+        "name": "Ficção Científica"
+    },
+    {
+        "id": 10770,
+        "name": "Televisivo"
+    },
+    {
+        "id": 53,
+        "name": "Terror"
+    },
+    {
+        "id": 10752,
+        "name": "Guerra"
+    },
+    {
+        "id": 37,
+        "name": "Faroeste"
+    }
+]
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
+// const acc = document.getElementById('acc') 
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+var selectedGenre = []
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML = '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id = genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if (selectedGenre.length == 0) {
+                selectedGenre.push(genre.id);
+            } else {
+                if (selectedGenre.includes(genre.id)) {
+                    selectedGenre.forEach((id, idx) => {
+                        if (id == genre.id) {
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                } else {
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getMovies(url_api + '&with_genres=' + encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t);
+    })
+}
+
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if (selectedGenre.length != 0) {
+        selectedGenre.forEach(id => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add('highlight');
+        })
+    }
+
+}
+
+function clearBtn() {
+    let clearBtn = document.getElementById('clear');
+    if (clearBtn) {
+        clearBtn.classList.add('highlight')
+    } else {
+
+        let clear = document.createElement('div');
+        clear.classList.add('tag', 'highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();
+            getMovies(url_api);
+        })
+        tagsEl.append(clear);
+    }
+
+}
+
+
+// getAcc();
+
+// function getAcc(){
+//     lastUrl = url;
+//     fetch(url).then(res => res.json()).then(data => {
+//         console.log(data.results)
+
+//     })
+// }
+
+getMovies(url_api);
+
+function getMovies(url) {
+    lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if (data.results.length !== 0) {
+            showMovies(data.results);
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+            current.innerText = currentPage;
+            if (currentPage <= 1) {
+                prev.classList.add('disabled');
+                next.classList.remove('disabled')
+            } else if (currentPage >= totalPages) {
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+            } else {
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled')
+            }
+            tagsEl.scrollIntoView({ behavior: 'smooth' })
+        } else {
+            main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+        }
+    })
+}
+
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const { title, poster_path, vote_average, overview, id } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path ? url_img + poster_path : "http://via.placeholder.com/1080x1580"}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>${title}</h3>
+                ${overview}
+                <br/> 
+                <button class="know-more" id="${id}">Saiba Mais</button
+            </div>       
+        `
+        main.appendChild(movieEl);
+        document.getElementById(id).addEventListener('click', () => {
+            console.log(id)
+            openNav(movie)
+        })
+    })
+}
+
+const overlayContent = document.getElementById('overlay-content');
+
+function openNav(movie) {
+    let id = movie.id;
+    fetch(url_base + '/movie/' + id + '/videos?' + api_key).then(res => res.json()).then(videoData => {
+        console.log(videoData);
+        if (videoData) {
+            document.getElementById("myNav").style.width = "100%";
+            if (videoData.results.length > 0) {
+                var embed = [];
+                var dots = [];
+                videoData.results.forEach((video, idx) => {
+                    let { name, key, site } = video
+
+                    if (site == 'YouTube') {
+
+                        embed.push(`
+              <iframe width="700" height="400" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          
+          `)
+
+                        dots.push(`
+              <span class="dot">${idx + 1}</span>
+            `)
+                    }
+                })
+
+                var content = `
+        <h1 class="no-results">${movie.original_title}</h1>
+        <br/>
+        
+        ${embed.join('')}
+        <br/>
+
+        <div class="dots">${dots.join('')}</div>
+        
+        `
+                overlayContent.innerHTML = content;
+                activeSlide = 0;
+                showVideos();
+            } else {
+                overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+            }
+        }
+    })
+}
+
+// fechar navegação dps do clique em saiba mais // encerrar video dps do x ??
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+var activeSlide = 0;
+var totalVideos = 0;
+
+function showVideos() {
+    let embedClasses = document.querySelectorAll('.embed');
+    let dots = document.querySelectorAll('.dot');
+
+    totalVideos = embedClasses.length;
+    embedClasses.forEach((embedTag, idx) => {
+        if (activeSlide == idx) {
+            embedTag.classList.add('show')
+            embedTag.classList.remove('hide')
+
+        } else {
+            embedTag.classList.add('hide');
+            embedTag.classList.remove('show')
+        }
+    })
+
+    dots.forEach((dot, indx) => {
+        if (activeSlide == indx) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active')
+        }
+    })
+}
+
+const leftArrow = document.getElementById('left-arrow')
+const rightArrow = document.getElementById('right-arrow')
+
+leftArrow.addEventListener('click', () => {
+    if (activeSlide > 0) {
+        activeSlide--;
+    } else {
+        activeSlide = totalVideos - 1;
+    }
+
+    showVideos()
+})
+
+rightArrow.addEventListener('click', () => {
+    if (activeSlide < (totalVideos - 1)) {
+        activeSlide++;
+    } else {
+        activeSlide = 0;
+    }
+    showVideos()
+})
+
+// insere as notas dos filmes classificado por cor 
+function getColor(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return "orange"
+    } else {
+        return 'red'
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    selectedGenre = [];
+    setGenre();
+    if (searchTerm) {
+        getMovies(url_search + '&query=' + searchTerm + url_lang)
+    } else {
+        getMovies(url_api);
+    }
+
+})
+// eventos pra mudar de pagina 
+prev.addEventListener('click', () => {
+    if (prevPage > 0) {
+        pageCall(prevPage);
+    }
+})
+
+next.addEventListener('click', () => {
+    if (nextPage <= totalPages) {
+        pageCall(nextPage);
+    }
+})
+
+function pageCall(page) {
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length - 1].split('=');
+    if (key[0] != 'page') {
+        let url = lastUrl + '&page=' + page
+        getMovies(url);
+    } else {
+        key[1] = page.toString();
+        let a = key.join('=');
+        queryParams[queryParams.length - 1] = a;
+        let b = queryParams.join('&');
+        let url = urlSplit[0] + '?' + b
+        getMovies(url);
+    }
+}
